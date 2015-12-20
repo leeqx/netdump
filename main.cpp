f07b2961570f24505c341d88efb076b8c9df65ba
liqingxin <lqx0830@hotmai.com>
2015-12-20 14:42:55 +0800

    添加redis协议解析

--- main.cpp
+++ main.cpp
@@ -2,111 +2,150 @@
 #include "myparse.hpp"
 #include "select.hpp"
 #include "filter.hpp"
+#include "redis_parse.hpp"
 
+enum EParseTpe
+{
+    EDEFAULT=0,
+    EREDIS=1,
+    EPROTOBUFER=2
+};
 int useage()
 {
-	fprintf(stdout,
-			"usage:\n"
-			"\t s-packet size \n"
-			"\t S-source ip \n"
-			"\t D-dest   ip \n"
-            "\t p-source port\n"
-            "\t d-dst    port\n"
-			"\t t-protocol type:tcp|udp|icmp \n"
-			"\t i-ether  lo|eth0|eth1 \n"
-			"\t c-max packet count \n");
-	return 0;
+    fprintf(stdout,
+     "usage:\n"
+     "\t s-packet size \n"
+     "\t S-source ip \n"
+     "\t D-dest   ip \n"
+     "\t p-source port\n"
+     "\t d-dst    port\n"
+     "\t t-protocol type:tcp|udp|icmp \n"
+     "\t i-ether  lo|eth0|eth1 \n"
+     "\t c-max packet count \n"
+     "\t u-use parse to pare packet\n");
+    return 0;
 }
 int main(int argc,char* argv[])
 {
-	if(argc < 2)
-	{
-		useage();
-		exit(-1);
-	}
-	int c = 0;
-	extern int optind,optopt;
-	extern char* optarg;
-	CFilter filter;
-	// s-packet size
-	// S-source ip
-	// D-dest   ip
-	// p-source port
-	// d-dst    port
-	// t-protocol type:tcp|udp|icmp
-	// i-ether  lo|eth0|eth1
-	// c-max packet count
-	// 
-	while((c=getopt(argc,argv,"s:S:D:t:i:c:p:d:")) != -1)
-	{
-		switch(c)
-		{
-			case 's':
-			{	
-				filter.SetPacketSize(atoi(optarg));
-				break;
-			}
-			case 'S':
-			{
-				filter.SetSrcIp(optarg);
-				break;
-			}
-			case 'D':
-			{
-				filter.SetDstIp(optarg);
-				break;
-			}
-			case 'p':
-			{
-				filter.SetSrcPort(atoi(optarg));
-				break;
-			}
-			case 'd':
-			{
-				filter.SetDstPort(atoi(optarg));
-				break;
+    if(argc < 2)
+    {
+        useage();
+        exit(-1);
+    }
+    int c = 0;
+    int parseType = 0;
+    extern char* optarg;
+    CFilter filter;
+    // s-packet size
+    // S-source ip
+    // D-dest   ip
+    // p-source port
+    // d-dst    port
+    // t-protocol type:tcp|udp|icmp
+    // i-ether  lo|eth0|eth1
+    // c-max packet count
+    // u-use parse to pare packet
+    // 
+    while((c=getopt(argc,argv,"s:S:D:t:i:c:p:d:u:")) != -1)
+    {
+        switch(c)
+        {
+            case 's':
+                {	
+                    filter.SetPacketSize(atoi(optarg));
+                    break;
+                }
+            case 'S':
+                {
+                    filter.SetSrcIp(optarg);
+                    break;
+                }
+            case 'D':
+                {
+                    filter.SetDstIp(optarg);
+                    break;
+                }
+            case 'p':
+                {
+                    filter.SetSrcPort(atoi(optarg));
+                    break;
+                }
+            case 'd':
+                {
+                    filter.SetDstPort(atoi(optarg));
+                    break;
+                }
+            case 't':
+                {
+                    if(strncmp(optarg,"tcp",strlen("tcp"))==0)
+                        filter.SetProtoType(0);
+                    else if(strncmp(optarg,"udp",strlen("udp")) == 0)
+                        filter.SetProtoType(1);
+                    else
+                    {
+                        fprintf(stderr,"prototype %s unsuport this version\n",optarg);
+                        exit(0);
+                    }
+                    break;
+                }
+            case 'i':
+                {
+                    if(strncmp(optarg,"any",strlen("any")) ==0)
+                    {
+                        fprintf(stderr,"current version unsport ether type any\n");
+                        exit(0);
+                    }	
+                    else
+                        filter.SetEtherType(optarg);
+                    break;
+                }
+            case 'c':
+                {
+                    filter.SetPacketCount(atoi(optarg));
+                    break;
+                }
+            case 'u':
+                {
+                    if(strncmp(optarg,"raw",strlen("raw")) == 0)
+                    {
+                        parseType = EDEFAULT;
+                    }
+                    else if(strncmp(optarg,"redis",strlen("redis")))
+                    {
+                        parseType = EREDIS; 
+                    }
+                    else
+                    {
+                        parseType = EDEFAULT;
+                    }
+
+                }
+            default:
+                {
+                    fprintf(stderr,"unsuport option\n");
+                    useage();
+                    break;
+                }
+        }
+    }
+
+    switch(parseType)
+    {
+        case EREDIS:
+            {
+                CNet<CSelect<CRedisParse> > mydump(filter.GetSrcPort());
+                mydump.InitNetRawSocket(filter,false,true,filter.GetEtherType());
+                break;
             }
-			case 't':
-			{
-				if(strncmp(optarg,"tcp",strlen("tcp"))==0)
-					filter.SetProtoType(0);
-				else if(strncmp(optarg,"udp",strlen("udp")) == 0)
-					filter.SetProtoType(1);
-				else
-				{
-					fprintf(stderr,"prototype %s unsuport this version\n",optarg);
-					exit(0);
-				}
-				break;
-			}
-			case 'i':
-			{
-				if(strncmp(optarg,"any",strlen("any")) ==0)
-				{
-					fprintf(stderr,"current version unsport ether type any\n");
-					exit(0);
-				}	
-                else
-                    filter.SetEtherType(optarg);
-				break;
-			}
-			case 'c':
-			{
-				filter.SetPacketCount(atoi(optarg));
-				break;
-			}
-			default:
-			{
-				fprintf(stderr,"unsuport option\n");
-				useage();
-				break;
-			}
-		}
-	}
+        case EDEFAULT:
+        default:
+            {
+                CNet<CSelect<CMyParse> > mydump(filter.GetSrcPort());
+                mydump.InitNetRawSocket(filter,false,true,filter.GetEtherType());
+                break;
+            }
 
-
-	CNet<CSelect<CMyParse> > mydump(filter.GetSrcPort());
-	//mydump.InitNetServerSocket(false,true);
-	mydump.InitNetRawSocket(filter,false,true,filter.GetEtherType());
+    }
+    return 0;
 }
 
---
+++ redis_parse.hpp
@@ -0,0 +1,123 @@
+#pragma once
+#include <string.h>
+#include <map>
+#include <algorithm>
+
+#include "net.hpp"
+#include "netparse.h"
+
+using namespace std::string;
+using namespace std::map;
+using namespace std::pair;
+
+struct redisPackPrint{
+    void operator ()(pair<string,string> data)
+    {
+        fprintf(stdout,"%s=%s\n",data->first.c_str(),data->second.c_str()) 
+    }
+};
+
+typedef struct redispack{
+    char nNumber;
+    map<string,string> kvs;
+}RedisPackage;
+
+class CRedisParse: public CNetPacketParse
+{
+    public:
+        CRedisParse(char* buffer,int len,CFilter filter):CNetPacketParse(buffer,len,filter)
+        {
+        }
+        /**
+         * 请求包:
+         *    * \r\n  $ \r\n .. $ \r\n \r\n
+         *    eg: *3 $3 SET $5 mykey $7 myvalue
+         *     "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n\r\n
+         * 应答包:
+         *   如果是单行回复，那么第一个字节是「+」
+         *   如果回复的内容是错误信息，那么第一个字节是「_」
+         *   如果回复的内容是一个整型数字，那么第一个字节是「:」
+         *   如果是bulk回复，那么第一个字节是「$」
+         *   如果是multi-bulk回复，那么第一个字节是「*」
+         */
+        virtual int32_t Parse()
+        {
+            if(this->m_state != 0)
+                return -1;
+            else if(this->m_isValidPackage == false)
+                return -2;
+
+            if(this->GetBufferLen() >0)
+            {
+                char* buffer = new char[m_buffer.size()+1];
+                if(buffer)
+                {
+                    memcpy(buffer,m_buffer.c_str(),m_buffer.size());
+                    switch((char)buffer)
+                    {
+                        case ':':
+                            {
+                                fprintf(stdout,"value=[%s]",buffer+1)
+                                break;
+                            }
+                        case '+':
+                            {
+                                fprintf(stdout,"value=[%s]",buffer+1)
+                                break;
+                            }
+                        case '-':
+                            {
+                                fprintf(stdout,"value=[%s]",buffer+1)
+                                break;
+                            }
+                        case '$':
+                            {
+                                fprintf(stdout,"value=[%s]",buffer+1)
+                                for(char*pos = strtok(buffer+1,"\r\n"),i=0;pos;pos=strtok(NULL,"\r\rn"),i++)
+                                {
+                                    if(i % 2 == 0)
+                                    {
+                                        fprintf(stdout,"len=%s\n",pos)
+                                    }
+                                    else
+                                    {
+                                        fprintf(stdout,"data=%s",pos)
+                                    }
+                                }
+                                break;
+                            }
+                        case "*":
+                            {
+                                RedisPackage redisVal;
+                                for(char *pos = strtok(buffer+1,"\r\n"),int i = 0;pos;pos=strtok(NULL,"\r\n"),i++)
+                                {
+                                    if( i % 2 == 0 && i == 0)
+                                    {
+                                        redisVal.insert(pair<string,string>("total",pos));
+                                    }
+                                    else if (i % 2 == 1)
+                                    {
+                                        redisVal.insert(pair<string,string>("len",pos))
+                                    }
+                                    else
+                                    {
+                                        redisVal.insert(pair<string,string>("value",pos))
+                                    }
+
+                                }
+                                for_each(redisVal.begine(),redisVal.end(),redisPackPrint());
+                                break;
+                            }
+                        default:
+                            LOGMSG("unexpected buffer");
+                    }
+                    delete [] buffer;
+                    buffer = NULL;
+                }
+                else
+                {
+                    LOGMSG("error: memory operator failed:")
+                }
+            }
+        }
+};
